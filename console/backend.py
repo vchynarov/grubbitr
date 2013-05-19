a0662cf33ba752a5ab59e2ba66253b04a3d611f6
Viktor Chynarov <viktor.chynarov@gmail.com>
2013-05-19 12:57:32 -0500

    Condensed backend

--- console/backend.py
+++ console/backend.py
@@ -1,9 +1,12 @@
 def rename(temp_names, temp_dictionary, old_name, new_name):
-	temp_dictionary[new_name] = temp_dictionary.pop(old_name)
-	name_line = temp_dictionary[new_name][0]
-	name_line = name_line.replace(old_name, new_name)
-	temp_dictionary[new_name][0] = name_line
+	"""
+	Renames an operating system. All configuration data
+	of the old system is moved to the new name.
+	"""
 
+	temp_dictionary[new_name] = temp_dictionary.pop(old_name)
+	name_line = temp_dictionary[new_name][0].replace(old_name, new_name)
+	temp_dictionary[new_name][0] = name_line 
 	temp_names[temp_names.index(old_name)] = new_name
 
 	return temp_names, temp_dictionary
